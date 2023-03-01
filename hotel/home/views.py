09f5c50fb9f80e5a735d3621c6cb2510d0663a7d
Prince JAT <69460330+prince-python@users.noreply.github.com>
2023-03-01 18:45:10 +0530

    Delete hotel directory

--- hotel/home/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from . serializers import HomeSerializer
-from rest_framework import viewsets
-from .models import *
-
-
-class HomeViewset(viewsets.ModelViewSet):
-    queryset = Home.objects.all()
-    serializer_class = HomeSerializer
